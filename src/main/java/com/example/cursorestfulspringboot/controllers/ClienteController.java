674a805d6f721f5666c77efa032d459e1bd1611e
Otavio <otaviotppc@gmail.com>
2020-08-31 20:36:34 -0300

    /clientes/id

--- src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
@@ -1,5 +1,10 @@
 package com.example.cursorestfulspringboot.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController{
 
-    @GetMapping("/cliente")
-    public String getCliente(){
-            return "Retorna todos os clientes.";
+    public List<Cliente> clientes;
+    @PostConstruct
+    public void init(){
+
+        
+        Cliente c1= new Cliente();
+        c1.id=1;
+        c1.nome="Jose";
+        c1.endereco= "Rua X, 99";
+        c1.saldo = 202.0;
+
+        Cliente c2= new Cliente();
+        c2.id=2;
+        c2.nome="Maria";
+        c2.endereco= "Rua Y, 99";
+        c2.saldo = 444.0;
+        
+        Cliente c3= new Cliente();
+        c3.id=3;
+        c3.nome="Cleiton";
+        c3.endereco= "Rua Z, 99";
+        c3.saldo = 555.0;
+
+        clientes = Arrays.asList(c1,c2,c3);
+
     }
-    @GetMapping("/cliente/{codigo}")
-    public String getClienteByCodigo(@PathVariable int codigo){
-        return "Vai retornar o cliente de codigo: " + codigo ;
+
+    @GetMapping("/clientes")
+    public List<Cliente> getCliente(){
+     return clientes;
+    }
+    @GetMapping("/clientes/{id}")
+    public Cliente getClienteByCodigo(@PathVariable int id){
+       Cliente cliente= null;
+
+       for(Cliente aux : clientes){
+            if(aux.id==id){
+                cliente = aux;
+                break;
+            }
+       }
+
+       return cliente;
     }
 
 }
